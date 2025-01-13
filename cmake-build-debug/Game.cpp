931c1d3b77b2fa1ec787b0b97dc9ef57d445f7db
Akia Samuel Chah <80036169+Sammi-Drums@users.noreply.github.com>
2025-01-13 18:04:51 +0100

    Changes Not going

---
+++ cmake-build-debug/Game.cpp
@@ -0,0 +1,9 @@
+//
+// Created by sammi on 13/01/2025.
+//
+
+#include "Game.h"
+
+namespace group2 {
+
+} // group2
---
+++ cmake-build-debug/Game.h
@@ -0,0 +1,16 @@
+//
+// Created by sammi on 13/01/2025.
+//
+
+#ifndef GAME_H
+#define GAME_H
+
+namespace group2 {
+class Game {};
+
+} // group2
+
+#endif //GAME_H
+
+
+
